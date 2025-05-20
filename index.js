7beb46f2fea619ee695de0b02501d77debd962c8
Musstafa <musta_tricolor@hotmail.com>
2025-05-20 11:36:17 -0300

    commit update

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
-winAndLose(200 , 20)
+winAndLose(103 , 2)
 let heroName = "Musstafá"
-let notification = ("The hero " + heroName + " has a balance " + sun + " victories and is at the level of " )
+let notification = ("The hero " + heroName + " has a balance " + sum + " victories and is at the level of " )
 
 function winAndLose(win, lose) {
-    return sun = win - lose;
+    return sum = win - lose;
 }
 
 switch (true) {
-    case sun <= 10:
+    case sum <= 10:
     console.log(notification + "Iron");
         
         break;
 }
 
 switch (true) {
-    case sun <= 20:
+    case sum <= 20:
     console.log(notification + "Bronze");
         
         break;
 }
 switch (true) {
-    case sun <= 50:
+    case sum <= 50:
     console.log(notification + "Silver");
         
         break;
 }
 switch (true) {
-    case sun <= 80:
+    case sum <= 80:
     console.log(notification + "Gold");
         
         break;
 }
 switch (true) {
-    case sun <= 90:
+    case sum <= 90:
     console.log(notification + "Diamond");
         
         break;
 }
 switch (true) {
-    case sun <= 100:
+    case sum <= 100:
     console.log(notification + "Legendary");
         
         break;
 }
 switch (true) {
-    case sun >= 101:
-    console.log(notification + "Immoprtal");
+    case sum >= 101:
+    console.log(notification + "Immortal");
         
         break;
 
