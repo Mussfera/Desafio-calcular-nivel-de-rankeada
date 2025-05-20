c0b5fba35c336d3003c1a387625a70575c114c24
Musstafa <musta_tricolor@hotmail.com>
2025-05-19 21:54:11 -0300

    last commit

---
+++ index.js
@@ -0,0 +1,54 @@
+winAndLose(200 , 20)
+let heroName = "Musstafá"
+let notification = ("The hero " + heroName + " has a balance " + sun + " victories and is at the level of " )
+
+function winAndLose(win, lose) {
+    return sun = win - lose;
+}
+
+switch (true) {
+    case sun <= 10:
+    console.log(notification + "Iron");
+        
+        break;
+}
+
+switch (true) {
+    case sun <= 20:
+    console.log(notification + "Bronze");
+        
+        break;
+}
+switch (true) {
+    case sun <= 50:
+    console.log(notification + "Silver");
+        
+        break;
+}
+switch (true) {
+    case sun <= 80:
+    console.log(notification + "Gold");
+        
+        break;
+}
+switch (true) {
+    case sun <= 90:
+    console.log(notification + "Diamond");
+        
+        break;
+}
+switch (true) {
+    case sun <= 100:
+    console.log(notification + "Legendary");
+        
+        break;
+}
+switch (true) {
+    case sun >= 101:
+    console.log(notification + "Immoprtal");
+        
+        break;
+
+    
+    
+}
